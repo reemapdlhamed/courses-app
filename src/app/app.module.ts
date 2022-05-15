9d5fea4569e407f56862da14bdf187e476f7fbf5
reemapdlhamed <93556835+reemapdlhamed@users.noreply.github.com>
2022-05-15 04:11:27 +0200

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExamComponent } from './exams/exam/exam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamComponent
   ],
   imports: [
     BrowserModule,
