943668561a774b9025c6d3da1c90e70295de6af1
reemapdlhamed <93556835+reemapdlhamed@users.noreply.github.com>
2022-05-15 17:50:14 +0200

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExamComponent } from './exams/exam/exam.component';
+import { ExamComponent } from './pages/exams/exam/exam.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExamComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ExamComponent, HeaderComponent,FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/exams/exam/exam.component.ts
+++ src/app/pages/exams/exam/exam.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-exam',
   templateUrl: './exam.component.html',
-  styleUrls: ['./exam.component.css']
+  styleUrls: ['./exam.component.css'],
 })
 export class ExamComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  isShown: boolean = false;
+  constructor() {}
+  toggle() {
+    this.isShown = !this.isShown;
+    console.log(this.isShown);
   }
-
+  ngOnInit(): void {}
 }
